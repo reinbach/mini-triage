d0095472a9c18c97597361be870da45b7a7afe54
Greg Reinbach <greg@reinbach.com>
2012-07-02 08:23:37 -0400

    Ensure uid assigned to users

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import gevent
+import uuid
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from gevent.pywsgi import WSGIServer
+
+from models import EventHandler, User
 
 app = Flask(__name__)
 app.debug = True
 
+users = {}
+event_handler = EventHandler()
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    """Set uuid for user and redirect to triage page"""
+    uid = uuid.uuid1()
+    user = users[u"{0}".format(uid)] = User()
+    event_handler.subscribe(user)
+    return redirect("/{0}/".format(uid))
+
+@app.route("/<uid>/")
+def triage(uid):
+    # Make sure user has uuid, otherwise redirect to home
+    if not users.get(uid, False):
+        return redirect(url_for("home"))
+    return render_template("index.html", data=event_handler, uid=uid)
 
 def main():
     # Setup server to handle webserver requests
