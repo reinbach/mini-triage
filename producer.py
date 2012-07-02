6e3b378090c977605fa4e963930e2773315ab6b1
Greg Reinbach <greg@reinbach.com>
2012-07-02 07:20:54 -0400

    Added simple data/message producer

---
+++ producer.py
@@ -0,0 +1,29 @@
+import json
+import random
+import time
+import zmq
+
+def event_producer():
+    """Produce events in a random fashion"""
+    print "Event producer is running, generating events..."
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://127.0.0.1:5000")
+
+    EVENT_MESSAGES = [
+        "Hello World!",
+        "Call me",
+        "Server is down",
+        "Need those reports in triplicate",
+    ]
+
+    while True:
+        # randomly select event message
+        random_event_message = random.sample(EVENT_MESSAGES, 1)[0]
+        random_time_delay = random.randint(1, 5)
+        socket.send(json.dumps(dict(message=random_event_message)))
+        print "Sent message: {0}".format(random_event_message)
+        time.sleep(random_time_delay)
+
+if __name__ == "__main__":
+    event_producer()
