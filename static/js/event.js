92479a4780e5bb114afbac1d671d24bcada0cea1
Greg Reinbach <greg@reinbach.com>
2012-07-05 08:38:54 -0400

    Started add hooks in event js to interact with socketio server

--- static/js/event.js
+++ static/js/event.js
@@ -1,28 +1,47 @@
 $(function() {
     // Open up a connection to our server
-    var socket = io.connect("", {'host': 'localhost', 'port': 9999});
+    var socket = io.connect();
 
     // Save our event placeholder
     var $placeholder = $('#placeholder');
 
     // Handle a new message/event?
-    socket.on('event_add', function(msg) {
-        var d = $.parseJSON(msg);
-        $placeholder.append("<div id='" + d.uid + "' class='well'>" + d.message + "</div>");
+    socket.on('event_add', function(data) {
+        var d = $.parseJSON(data);
+        $placeholder.append(
+	    "<div id='" + d.uid +
+		"' class='event well'><button class='close' data-dismiss='alert'>&times;</button>" + d.message +
+		"</div>"
+	);
     });
 
     // Handle updating of event
-    socket.on('event_update', function(msg) {
-	var d = $.parseJSON(msg);
+    socket.on('event_update', function(data) {
+	var d = $.parseJSON(data);
 	// update event information
 	// move to relevant category/placeholder
+    });
+
+    // Handle deleting of event
+    $(".event").alert();
+    $(".event").live("closed", function() {
+	// let server know that event has been deleted
+	//test
+	console.log("delete event callback")
+	socket.emit("delete", {'event_id': $(this).attr("id")});
+    });
+    socket.on('event_delete', function(data) {
+	var d = $.parseJSON(data);
+	//test
+	console.log(d);
+	$("#" + d.uid).hide();
     });
 
     // Just update our conn_status field with the connection status
     socket.on('connect', function() {
         $('#conn_status').html('<b>Connected</b>');
 	$('#conn_status').attr("class", "label label-success")
-	// this is the call that streams the sine wave data
+	// this is the call that streams the events being added
 	socket.emit('stream', '');
     });
     socket.on('error', function() {
