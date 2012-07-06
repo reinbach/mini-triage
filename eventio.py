b9510feef8a85b6cced2246e4cbb20897ffe38b5
Greg Reinbach <greg@reinbach.com>
2012-07-06 07:51:35 -0400

    moved event streaming function into a spawned wrapper

--- eventio.py
+++ eventio.py
@@ -6,18 +6,20 @@ from socketio.namespace import BaseNamespace
 from socketio.mixins import BroadcastMixin
 
 class EventIOApp(BaseNamespace, BroadcastMixin):
-    """Stream events"""
-    def on_stream(self, msg):
+    def subscribe(self):
         context = zmq.Context()
         sock = context.socket(zmq.SUB)
         sock.setsockopt(zmq.SUBSCRIBE, "")
         sock.connect("tcp://127.0.0.1:5000")
-        print "Connected, ready to stream events..."
+
         while True:
             msg = sock.recv()
             event = self.request.add(msg)
             self.broadcast_event('event_add', json.dumps(event.__dict__))
             gevent.sleep(0.1)
+
+    def on_stream(self, msg):
+        self.spawn(self.subscribe)
 
     def on_update(self, data):
         event = self.request.update(data.get('event_id'), data)
