62e7711a6a55f0e29a1f66837216f03182a06a95
Greg Reinbach <greg@reinbach.com>
2012-07-06 15:03:55 -0400

    Socket io now returns render html for event items

--- eventio.py
+++ eventio.py
@@ -2,10 +2,26 @@ import gevent
 import json
 import zmq
 
+from jinja2 import Environment, PackageLoader
 from socketio.namespace import BaseNamespace
 from socketio.mixins import BroadcastMixin
 
+from models import CATEGORIES, STATUSES
+
 class EventIOApp(BaseNamespace, BroadcastMixin):
+
+    def __init__(self, *args, **kwargs):
+        super(EventIOApp, self).__init__(*args, **kwargs)
+        self.env = Environment(loader=PackageLoader('app', 'templates'))
+
+    def render_event(self, event):
+        template = self.env.get_template("event.html")
+        return template.render(
+            event=event,
+            categories=CATEGORIES,
+            statuses=STATUSES
+        )
+
     def subscribe(self):
         context = zmq.Context()
         sock = context.socket(zmq.SUB)
@@ -15,7 +31,7 @@ class EventIOApp(BaseNamespace, BroadcastMixin):
         while True:
             msg = sock.recv()
             event = self.request.add(msg)
-            self.broadcast_event('event_add', json.dumps(event.__dict__))
+            self.broadcast_event('event_add', self.render_event(event))
             gevent.sleep(0.1)
 
     def on_stream(self, msg):
@@ -23,7 +39,7 @@ class EventIOApp(BaseNamespace, BroadcastMixin):
 
     def on_update(self, data):
         event = self.request.update(data.get('event_id'), data)
-        self.broadcast_event_not_me('event_update', json.dumps(event.__dict__))
+        self.broadcast_event_not_me('event_update', event.to_json())
 
     def on_delete(self, data):
         event_id = data.get('event_id')
